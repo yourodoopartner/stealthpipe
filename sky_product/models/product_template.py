bb6ca47ec7e4887e25cda11095b08eb91f96e183
Anup Chavda(Skyscend) <anup.chavda@skyscendbs.com>
2023-01-27 20:40:26 +0530

    [IMP]sky_product:Added 3 decimal points for wall thickness

--- sky_product/models/product_template.py
+++ sky_product/models/product_template.py
@@ -5,8 +5,31 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     diameter = fields.Float(string='Diameter')
-    wall_thickness = fields.Float(string='Wall Thickness')
+    wall_thickness = fields.Float(string='Wall Thickness', digits=(16, 3))
     length = fields.Float(string='Length')
+
+    def update_sales_description(self):
+        self.description_sale = ''
+        if self.diameter != 0.0:
+            self.description_sale += 'Diameter: ' + str(self.diameter) + ', '
+
+        if self.wall_thickness != 0.0:
+            self.description_sale += 'Wall Thickness: ' + str(self.wall_thickness) + ', '
+
+        if self.length != 0.0:
+            self.description_sale += 'Length: ' + str(self.length)
+
+    @api.model_create_multi
+    def create(self, vals):
+        res = super(ProductTemplate, self).create(vals)
+        res.update_sales_description()
+        return res
+
+    def write(self, vals):
+        res = super(ProductTemplate, self).write(vals)
+        if 'name' in vals or 'diameter' in vals or 'wall_thickness' in vals or 'length' in vals:
+            self.update_sales_description()
+        return res
 
 
 class ProductProduct(models.Model):
