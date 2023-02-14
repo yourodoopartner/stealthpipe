cd60301656c8cd99c7d35aeceb16a31e2479ec52
Anup Chavda(Skyscend) <anup.chavda@skyscendbs.com>
2023-02-14 19:57:23 +0530

    [ADD]sky_stock_warehouse_by_user:Added additional module for the warehouse features

---
+++ sky_stock_warehouse_by_user/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+##########################################################################################
+#
+#    Copyright (C) 2019 Skyscend Business Solutions (https://www.skyscendbs.com)
+#    Copyright (C) 2020 Skyscend Business Solutions Pvt. Ltd. (https://www.skyscendbs.com)
+#
+##########################################################################################
+{
+    'name': ' Sky Stock Warehouse By User',
+    'version': '15.0.0.1',
+    'category': 'Stock',
+    'license': 'AGPL-3',
+    'description': """
+     This module is used to show Delivery orders based on group.
+     """,
+    'author': 'Skyscend Business Solutions Pvt. Ltd.',
+    'website': 'https://www.skyscendbs.com',
+    'depends': ['stock'],
+    'data': [
+        'security/stock_warehouse_security.xml',
+        'views/res_users_view.xml',
+    ],
+    'installable': True,
+    'auto-install': True
+}
