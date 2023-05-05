de97217ca4f652db9dd2f4f31218db2cdb1e3bba
Anup Chavda(Skyscend) <anup.chavda@skyscendbs.com>
2023-05-05 12:42:25 +0530

    [IMP]sky_product:Improved the script to not create variants and keeping the separate product for each variant

--- sky_product/models/product_template.py
+++ sky_product/models/product_template.py
@@ -56,3 +56,38 @@ class ProductTemplate(models.Model):
                                 UPDATE product_product SET weight = %s WHERE product_tmpl_id = %s
                             """
                     self._cr.execute(query, (tuple([row[5]]), tuple([product_data_dict.get(row[1])])))
+
+    def import_product_template(self):
+        product_name_list = ['Grade:A500 & Range: R1 – 16’ / 24’', 'Grade:A500 & Range: R2 – 25’ / 34’',
+                             'Grade:A500 & Range: R3 – 35’ / 48’', 'Grade:A500 & Range: R4 – 49’ / 62’',
+                             'Grade:A500 & Range: HR', 'Grade:A500 & Range: Shorts – 2’ / 15’',
+
+                             'Grade:A-252 & Range: R1 – 16’ / 24’', 'Grade:A-252 & Range: R2 – 25’ / 34’',
+                             'Grade:A-252 & Range: R3 – 35’ / 48’', 'Grade:A-252 & Range: R4 – 49’ / 62’',
+                             'Grade:A-252 & Range: HR', 'Grade:A-252 & Range: Shorts – 2’ / 15’',
+
+                             'Grade:A-53 & Range: R1 – 16’ / 24’', 'Grade:A-53 & Range: R2 – 25’ / 34’',
+                             'Grade:A-53 & Range: R3 – 35’ / 48’', 'Grade:A-53 & Range: R4 – 49’ / 62’',
+                             'Grade:A-53 & Range: HR', 'Grade:A-53 & Range: Shorts – 2’ / 15’']
+        count = 0
+        file_path = get_module_path('sky_product')
+        with open(file_path+'/static/Product Template.csv', 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            header = next(csvreader)
+            for row in csvreader:
+                for new_name in product_name_list:
+                    product_dict = {
+                        'name': row[1] +' '+ new_name,
+                        'uom_id': 18,
+                        'uom_po_id': 18,
+                        'default_code': row[2],
+                        'detailed_type': 'product',
+                        'invoice_policy': 'delivery',
+                        'purchase_method': 'receive',
+                        'diameter': row[3],
+                        'wall_thickness': row[4],
+                        'weight': row[5],
+                    }
+                    self.create(product_dict)
+                    count += 1
+
