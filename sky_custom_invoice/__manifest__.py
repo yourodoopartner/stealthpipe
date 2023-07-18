1f7300a50d4144931f1d41066ad1b9a212a23fdd
Anup Chavda(Skyscend) <anup.chavda@skyscendbs.com>
2023-07-18 18:03:52 +0530

    [ADD]sky_custom_invoice:Added the module for Custom Report

---
+++ sky_custom_invoice/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Skyscend Business Solutions
+#    Copyright (C) 2019 (http://www.skyscendbs.com)
+#    Skyscend Business Solutions Pvt. Ltd.
+#    Copyright (C) 2020 (http://wee.skyscendbs.com)
+#
+##############################################################################
+{
+    'name': 'Sky Custom Invoice',
+    'version': '15.0.0.1',
+    'category': 'Others',
+    'license': 'AGPL-3',
+    'description': """
+    This module is used for Customs Invoice report
+    """,
+    'author': 'Skyscend Business Solutions',
+    'website': 'http://www.skyscendbs.com',
+    'depends': ['purchase',],
+    'data': [
+        'views/template_view.xml',
+        'views/canada_custom_report_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
