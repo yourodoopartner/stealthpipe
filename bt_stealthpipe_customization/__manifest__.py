fb494f95338e7ce5f68a07ac64f8f364b9667115
Romy Roy <romyroy@broadtech-innovations.com>
2024-01-10 14:01:31 +0530

    added a new field in stock move line

---
+++ bt_stealthpipe_customization/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'BT Stealthpipe Customization',
+    'version': '1.0.0',
+    'summary': 'BT Stealthpipe Configuration',
+    'description': """
+        BT Stealthpipe Configuration.
+    """,
+    'author': 'BroadTech IT Solutions Pvt Ltd',
+    'website': 'http://www.broadtech-innovations.com',
+    'depends': ['stock','base','sale','account','sale_management','sky_stock'],
+    'data': [
+        'views/stock_view.xml',
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ bt_stealthpipe_customization/models/stock_move_line.py
@@ -0,0 +1,18 @@
+from odoo import models, fields, api, _
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+
+    diameter = fields.Float('Diameter', related='product_id.diameter', store=True)
+
+
+
+
+
+class StockValuationLayer(models.Model):
+    _inherit = 'stock.valuation.layer'
+
+    diameter = fields.Float('Diameter', related='product_id.diameter', store=True)
+
+
